5176a6a20cb9f3ecfc23c1afa871d86cc56ff6f5
ct0202 <arman.jumashev.dev@gmail.com>
2025-04-19 23:52:27 +0500

    fix baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5002/api/admin',
-  baseURL: 'https://api.buziak.online/api/admin',
+  baseURL: 'https://api.buziak.online',
   headers: {
     'Content-Type': 'application/json',
   },
